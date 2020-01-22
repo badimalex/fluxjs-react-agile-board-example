cc16ddae392679dedfa08ff058b915553811c89e
Dmitry Badichan <badimalex@gmail.com>
2020-01-22 14:44:07 +0100

    Add file

---
+++ server.js
@@ -0,0 +1,17 @@
+var path = require('path');
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.static(__dirname + '/dist'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on prot 3000');
+});
